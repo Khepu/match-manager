7fafeab1387a22c3cba627dfd6265e07e8e864b7
Khepu <makris.giwrgos1234@gmail.com>
2022-06-04 22:14:28 +0300

    Feature: Create findOne method for all entities

--- match-manager-repository/src/main/java/com/gmakris/matchmanager/repository/GenericRepository.java
+++ match-manager-repository/src/main/java/com/gmakris/matchmanager/repository/GenericRepository.java
@@ -1,5 +1,6 @@
 package com.gmakris.matchmanager.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import com.gmakris.matchmanager.entity.model.AbstractEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface GenericRepository<Entity extends AbstractEntity> extends JpaRepository<Entity, UUID> {
+
+    Optional<Entity> findOneById(UUID entityId);
 }
