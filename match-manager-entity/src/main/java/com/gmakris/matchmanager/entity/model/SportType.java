17a82040bf7f5c590ade951487723872b00b54d1
Khepu <makris.giwrgos1234@gmail.com>
2022-06-04 22:42:33 +0300

    Feature: Add reverse enum match

--- match-manager-entity/src/main/java/com/gmakris/matchmanager/entity/model/SportType.java
+++ match-manager-entity/src/main/java/com/gmakris/matchmanager/entity/model/SportType.java
@@ -1,12 +1,16 @@
 package com.gmakris.matchmanager.entity.model;
 
+import java.util.Arrays;
+
 public enum SportType {
-    FOOTBALL(1),
-    BASKETBALL(2);
+    FOOTBALL,
+    BASKETBALL;
 
-    final int ordinal;
-
-    SportType(final int ordinal) {
-        this.ordinal = ordinal;
+    public static SportType match(final int ordinal) {
+        return Arrays.stream(SportType.values())
+            .filter(sportType -> sportType.ordinal() == ordinal)
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException(
+                "Could not match '" + ordinal + "' to any sport type!"));
     }
 }
